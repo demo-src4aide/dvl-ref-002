7c23479fdd9d9e04b35aec44f75c16d96d0910c9
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-04-10 14:39:58 -0400

    Initial commit

---
+++ app/src/main/java/org/metatemple/smpl/dt20140410/n02/MainActivity.java
@@ -0,0 +1,14 @@
+package org.metatemple.smpl.dt20140410.n02;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
