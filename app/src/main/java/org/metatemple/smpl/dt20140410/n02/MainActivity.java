43b66ab61d1e3c9e3bdfd19f702462b4485fb422
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-04-10 21:39:33 -0400

    The function to change the text is implemented.

--- app/src/main/java/org/metatemple/smpl/dt20140410/n02/MainActivity.java
+++ app/src/main/java/org/metatemple/smpl/dt20140410/n02/MainActivity.java
@@ -2,6 +2,8 @@ package org.metatemple.smpl.dt20140410.n02;
 
 import android.app.*;
 import android.os.*;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +12,21 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		this.set_the_txt_change();
     }
+	
+	public void set_the_txt_change ()
+	{
+		// This is where we call findViewByID to get
+		// a TextView object referencing the text
+		// display area who's text we wish to change.
+		TextView tx_slot = (TextView)findViewById(R.id.hello_text);
+		
+		// And here, we call the TextView's method to
+		// change the text displayed in that text
+		// display. Clearly, referencing a string
+		// in the string XML file is easier than
+		// getting the TextView itself.
+		tx_slot.setText(R.string.hello_out);
+	}
 }
